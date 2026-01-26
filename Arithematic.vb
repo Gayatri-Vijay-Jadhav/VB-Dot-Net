51301a4c278f103166c530b68ed3a92fea3cc629
Gayatri Vijay Jadhav <gvj6705@gmail.com>
2026-01-26 19:20:48 +0530

    Add files via upload

---
+++ Arithematic.vb
@@ -0,0 +1,31 @@
+Module Arithematic
+
+    Sub Main()
+
+        Dim A As Integer = 20
+        Dim B As Integer = 10
+        Dim C As Double
+
+        C = A + B
+        Console.WriteLine("Addition: " & C)
+
+        C = A - B
+        Console.WriteLine("Subtraction: " & C)
+
+        C = A * B
+        Console.WriteLine("Multiplication: " & C)
+
+        C = A / B
+        Console.WriteLine("Division: " & C)
+
+        C = A Mod B
+        Console.WriteLine("Modulus: " & C)
+
+        C = A ^ B
+        Console.WriteLine("Power: " & C)
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
